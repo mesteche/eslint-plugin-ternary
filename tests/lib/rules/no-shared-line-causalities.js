c9d0b7f4bbbf39dc791b3491c00e135419cbd63d
Estephe Bouvet <meztchek@gmail.com>
2021-03-28 22:39:25 +0200

    ✨ add no-shared-line-causality rule

---
+++ tests/lib/rules/no-shared-line-causalities.js
@@ -0,0 +1,34 @@
+/**
+ * @fileoverview keep each pair of test + consequent on a separate line
+ * @author Estephe Bouvet
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var rule = require("../../../lib/rules/no-shared-line-causalities"),
+
+    RuleTester = require("eslint").RuleTester;
+
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+var ruleTester = new RuleTester();
+ruleTester.run("no-shared-line-causalities", rule, {
+    valid:   [
+        { code: "test1 ? consequent1 :\n test2 ? consequent2 :\n 'default'" },
+        { code: "test1 ? consequent1 :// endline comment\n test2 ? consequent2 :/* another comment*/\n 'default'" },
+      ],
+      invalid: [{
+        code: "test1 ? consequent1 : test2\n ? consequent2 : 'default'",
+        errors: [
+          { message: 'Don\'t share causality line' },
+          { message: 'Don\'t share causality line' },
+        ],
+        output: "test1 ? consequent1 : \ntest2\n ? consequent2 : \n'default'",
+      }],
+});
